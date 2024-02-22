6b993f90599ae535d07f76363a802161113e136a
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-22 23:39:51 +0500

    added Assignment 1

---
+++ Assignment 1/message.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Eric";
+console.log("hey ".concat(name, ",would you like to learn some python today?"));
---
+++ Assignment 1/message.ts
@@ -0,0 +1,10 @@
+let name = "Eric"
+console.log(`hey ${name},would you like to learn some python today?`)
+
+
+
+
+
+
+
+export{}
