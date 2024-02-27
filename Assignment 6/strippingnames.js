8822783a02e158e7666efdcded8ca4f6fa65dcff
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-27 10:35:09 +0500

    added Assignment 6

---
+++ Assignment 6/strippingnames.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "\t \n \n UMAIMA \n \t \n";
+console.log(name);
+var name2 = "UMAIMA";
+name2.trim();
+console.log(name2);
---
+++ Assignment 6/strippingnames.ts
@@ -0,0 +1,9 @@
+let name: string = "\t \n \n UMAIMA \n \t \n"
+console.log(name)
+let name2: string = "UMAIMA"
+name2.trim()
+console.log(name2)
+
+
+
+export{}
