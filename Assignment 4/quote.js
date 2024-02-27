24c7e6e915d62a7c29726e470d9e7772b0db6bee
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-27 10:22:35 +0500

    added Asiignment 4

---
+++ Assignment 4/quote.js
@@ -0,0 +1,8 @@
+/*let person = "Ali ibn Abi Talib"
+console.log(`${person} once said, "The disease of the heart is worse than the disease of body"`) */
+var a = 2;
+var b = 3;
+var c = 4;
+var d = 5;
+var num = a + b * d - c % a;
+console.log(num);
---
+++ Assignment 4/quote.ts
@@ -0,0 +1,2 @@
+let person = "Ali ibn Abi Talib"
+console.log(`${person} once said, "The disease of the heart is worse than the disease of body"`) 
