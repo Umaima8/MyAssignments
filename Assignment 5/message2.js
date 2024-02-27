b1fb627a0138dcd159aaff19ac138c46370a986f
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-27 10:33:57 +0500

    added Assignment 5

---
+++ Assignment 5/message2.js
@@ -0,0 +1,3 @@
+var person = "Ali Bin Abi Talib";
+var quote = "\"The disease of heart is worse than the disease of body\"";
+console.log("".concat(person) + "once  said , " + "".concat(quote));
---
+++ Assignment 5/message2.ts
@@ -0,0 +1,6 @@
+let person: string  = "Ali Bin Abi Talib"
+let quote = `"The disease of heart is worse than the disease of body"`
+console.log(`${person}`  +  "once  said , "  +   `${quote}`)
+
+
+
