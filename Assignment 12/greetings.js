69835c3ca3c1c0a6d18bb2dd79673593b2e916df
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-28 23:47:49 +0500

    added Assignment 12

---
+++ Assignment 12/greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var names = ["Umaima", "Tayyaba", "Mina", "Alishba"];
+//Using forEach as it iterates through each element in an array
+names.forEach(function (names) {
+    console.log("I wish you nothing but success ".concat(names, "!"));
+});
---
+++ Assignment 12/greetings.ts
@@ -0,0 +1,10 @@
+let names: string[] = ["Umaima", "Tayyaba","Mina", "Alishba"]
+
+//Using forEach as it iterates through each element in an array
+
+names.forEach((names) => {
+    console.log(`I wish you nothing but success ${names}!`)
+})
+
+
+export{}
