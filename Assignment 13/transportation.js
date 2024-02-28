324cdf7029fe6d074566dd0730672a3baac19f4e
UmaimaFareed <umaimafaird88@gmail.com>
2024-02-28 23:48:49 +0500

    added Assignment 13

---
+++ Assignment 13/transportation.js
@@ -0,0 +1,5 @@
+var cars = ["Audi", "BMW", "Ford", "Porsche"];
+console.log("I have always admired the sleek design of ".concat(cars[0]));
+console.log("Driving a ".concat(cars[1], " would be a thrilling experience"));
+console.log("".concat(cars[2], " vehicles have a rich history and offer practicality for everyday use"));
+console.log("Owning a ".concat(cars[3], " would be a dream come true!"));
---
+++ Assignment 13/transportation.ts
@@ -0,0 +1,5 @@
+const cars: string[] = ["Audi", "BMW", "Ford", "Porsche"]
+console.log(`I have always admired the sleek design of ${cars[0]}` )
+console.log(`Driving a ${cars[1]} would be a thrilling experience`)
+console.log(`${cars[2]} vehicles have a rich history and offer practicality for everyday use`)
+console.log(`Owning a ${cars[3]} would be a dream come true!`)
