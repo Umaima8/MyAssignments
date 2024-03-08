a64ce37981cecf876d0064b4d343cd6c9cc8914b
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-09 00:22:45 +0500

    added Assignment 19

---
+++ Assignment 19/guests.js
@@ -0,0 +1,4 @@
+var guests = ["Ghalib", "Iqbal", "Mir Taqi", "Mir Dard"];
+guests[3] = "Parveen Shakir";
+//printing the list of guests invited to dinner
+console.log("\"there are ".concat(guests.length, " coming to dinner tonight"));
---
+++ Assignment 19/guests.ts
@@ -0,0 +1,8 @@
+let guests: string[] = ["Ghalib", "Iqbal" , "Mir Taqi","Mir Dard"]
+
+guests[3] = "Parveen Shakir"
+
+
+//printing the list of guests invited to dinner
+
+console.log(`"there are ${guests.length} people  coming to dinner tonight`)
