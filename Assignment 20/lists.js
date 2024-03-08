3ec7485e3e130c1c647e29c167169a54a7058c22
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-09 00:23:35 +0500

    added Assignment 20

---
+++ Assignment 20/lists.js
@@ -0,0 +1,5 @@
+var rivers = ["Indus", "Hub", "Chenab", "Sutlej", "Jhelum"];
+//printing a list of rivers
+for (var i = 0; i < rivers.length; i++) {
+    console.log(rivers[i]);
+}
---
+++ Assignment 20/lists.ts
@@ -0,0 +1,6 @@
+let rivers: string[] = ["Indus", "Hub", "Chenab", "Sutlej", "Jhelum"]
+//printing a list of rivers
+
+for(let i = 0; i<rivers.length; i++) {
+    console.log(rivers[i])
+}
