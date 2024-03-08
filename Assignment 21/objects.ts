8f69e3baa7645d89e6945f07fa9bd3f7d03efb2f
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-09 00:24:37 +0500

    added Assignment 21

---
+++ Assignment 21/objects.ts
@@ -0,0 +1,17 @@
+type user =   {
+    name: string
+    age: number
+    education: string
+    gender: string
+}
+
+let info: user = {
+    name: "umaima",
+    age: 18,
+    education: "2nd year",
+    gender: "female"
+}
+
+
+
+
