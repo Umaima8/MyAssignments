2acccab3ca35a4513e9497c7dcb7242f8f2d5c71
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-09 00:26:42 +0500

    added Assignment 23

---
+++ Assignment 23/tests.js
@@ -0,0 +1,40 @@
+//first test
+var num = 7;
+console.log("\"is ".concat(num, " = odd? I predict True\""));
+console.log(num == 7);
+//second test 
+var num2 = 6;
+console.log("\"is ".concat(num2, " = even?I predict true\""));
+console.log(num2 % 2 == 0);
+//third test
+var num3 = 2;
+console.log("\"is ".concat(num3, " = prime?I predict true\""));
+console.log(num3 == 2);
+//fourth test
+var letter = 'a';
+console.log("\"is ".concat(letter, " = vowel? I predict true"));
+console.log(letter == 'a');
+//fifth test
+var letter2 = 'b';
+console.log("\"is ".concat(letter2, " = consonant? I predict true"));
+console.log(letter2 == 'b');
+//sixth test
+var car = 'car';
+console.log("\"is ".concat(car, " = auto?I predict false\" "));
+console.log(car == 'manual');
+//seventh test
+var bike = 'bike';
+console.log("\"is ".concat(bike, " = Honda?I predict false\""));
+console.log(bike == 'Yamaha');
+//eigth test
+var school = "school";
+console.log("\"is ".concat(school, " = English Medium?I predict false\""));
+console.log(school == "Urdu Medium");
+//ninth test
+var footwear = 'shoes';
+console.log("\"is ".concat(footwear, " = Nike ?I predict false\""));
+console.log(footwear == 'Addidas');
+//tenth test
+var brand = 'perfume';
+console.log("\"is ".concat(brand, " = Aramis?I predict false\""));
+console.log(brand == "Blue lady");
---
+++ Assignment 23/tests.ts
@@ -0,0 +1,65 @@
+//first test
+
+let num: number = 7
+console.log(`"is ${num} = odd? I predict True"`)
+console.log(num==7)
+
+//second test 
+
+let num2:number = 6
+console.log(`"is ${num2} = even?I predict true"`)
+console.log(num2 % 2 == 0)
+
+
+//third test
+
+let num3: number = 2
+console.log(`"is ${num3} = prime?I predict true"`)
+console.log(num3==2)
+
+//fourth test
+
+let letter: string = 'a'
+console.log(`"is ${letter} = vowel? I predict true`)
+console.log(letter=='a')
+
+//fifth test
+
+let letter2: string = 'b'
+console.log(`"is ${letter2} = consonant? I predict true`)
+console.log(letter2=='b')
+
+//sixth test
+
+let car: string = 'car'
+console.log(`"is ${car} = auto?I predict false" `)
+console.log(car == 'manual')
+
+//seventh test
+
+let bike: string = 'bike'
+console.log(`"is ${bike} = Honda?I predict false"`)
+console.log(bike == 'Yamaha')
+
+//eigth test
+
+let school: string = "school"
+console.log(`"is ${school} = English Medium?I predict false"`)
+console.log(school == "Urdu Medium")
+
+//ninth test
+
+let footwear: string = 'shoes'
+console.log(`"is ${footwear} = Nike ?I predict false"`)
+console.log(footwear == 'Addidas')
+
+//tenth test
+
+let brand: string = 'perfume'
+console.log(`"is ${brand} = Aramis?I predict false"`)
+console.log(brand == "Blue lady")
+
+
+
+
+
