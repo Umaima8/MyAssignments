127f50b48137f0b9f2dc400bf9acf2bdd0793550
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-12 09:21:39 +0500

    added Assignment 25

---
+++ Assignment 25/game.ts
@@ -0,0 +1,13 @@
+let  alien_color =  "green"
+
+alien_color = 'green'
+
+if(alien_color === 'green') {
+    console.log("Congrats!You have earned five extra points")
+}
+
+alien_color = 'yellow'
+
+if (alien_color === "green") {
+    console.log("Congrats!You have earned five extra points")
+}
