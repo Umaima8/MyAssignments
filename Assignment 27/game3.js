a51aba0f3a8934c73d45af067a6aa4444a2a34f0
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-12 09:26:16 +0500

    added Assignment 27

---
+++ Assignment 27/game3.js
@@ -0,0 +1,33 @@
+var alien = 'green';
+//printing the msg for the green alien
+if (alien === 'green') {
+    console.log("You have earned five points");
+}
+else if (alien === 'yellow') {
+    console.log("You have earned 10 points");
+}
+else {
+    console.log("You have earned 15 points");
+}
+//now for the yellow alien
+alien = 'yellow';
+if (alien === 'green') {
+    console.log("You have earned five points");
+}
+else if (alien === 'yellow') {
+    console.log("You have earned 10 points");
+}
+else {
+    console.log("You have earned 15 points");
+}
+//now for the red alien
+alien = 'red';
+if (alien === 'green') {
+    console.log("You have earned five points");
+}
+else if (alien === 'yellow') {
+    console.log("You have earned 10 points");
+}
+else {
+    console.log("You have earned 15 points");
+}
