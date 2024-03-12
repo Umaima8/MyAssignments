de8bda072594ee873e919c901fe7d8cd878f64b2
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-12 09:22:35 +0500

    added Assignment 26

---
+++ Assignment 26/game2.js
@@ -0,0 +1,16 @@
+var alien_color = 'green';
+//first we will run the if statement
+if (alien_color === 'green') {
+    console.log("you have earned five points for killing an alien");
+}
+else {
+    console.log("You have earned 10 points");
+}
+//now,we will run the else statement
+alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("you have earned five points for killing an alien");
+}
+else {
+    console.log("You have earned 10 points");
+}
