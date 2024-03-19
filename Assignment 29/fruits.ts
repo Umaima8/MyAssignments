1a889096844fa56134ae0be4458babead0873632
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-19 08:39:07 +0500

    added Assignment 29

---
+++ Assignment 29/fruits.ts
@@ -0,0 +1,37 @@
+let favourite_fruits: string[] =  ["Apple", "Orange", "Strawberry" , "Banana", "kiwi"]
+
+//checking for Apple
+
+if (favourite_fruits.indexOf("Apple") !== -1) {
+    console.log("Apple is my favourite fruit")
+}
+
+//checking for Orange
+
+if (favourite_fruits.indexOf("Orange") !== -1) {
+    console.log("Orange is my favourite fruit")
+}
+
+
+//checking for Strawberry
+
+if (favourite_fruits.indexOf("Strawberry") !== -1) {
+    console.log("Strawberry is my favourite fruit")
+}
+
+
+//checking for banana
+
+
+if (favourite_fruits.indexOf("Banana") !== -1) {
+    console.log("Banana is my favourite fruit")
+}
+
+
+//checking for kiwi
+
+
+
+if (favourite_fruits.indexOf("kiwi") !== -1) {
+    console.log("kiwi is my favourite fruit")
+}
