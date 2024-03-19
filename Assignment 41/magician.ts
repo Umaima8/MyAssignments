f23932ef884e5862049447068122dbb4124da006
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-19 22:18:24 +0500

    added Assignment 41

---
+++ Assignment 41/magician.ts
@@ -0,0 +1,13 @@
+
+
+function show_magicians (magicians: string[]): void {
+      
+    for (let magician of magicians) {
+        console.log(magician)
+    }
+
+}
+
+let Mymagicians: string[]  = ["Harry Potter", "Tom Riddle", "Draco Malfoy", "Hermione Granger"]
+
+show_magicians(Mymagicians)
