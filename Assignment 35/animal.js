d7da5167cc5d8eb6fea1e3da28e28410a5c3cc7c
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-19 09:03:37 +0500

    added Assignment 35

---
+++ Assignment 35/animal.js
@@ -0,0 +1,7 @@
+var animals = ["lion", "Tiger", "Cheetah"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("\"".concat(animal, " is a carnivorous animal\""));
+}
+console.log("\"".concat(animals, " are carnivorous\""));
+console.log("Any of these animals  would make a great pet");
---
+++ Assignment 35/animal.ts
@@ -0,0 +1,12 @@
+let animals: string[] = ["lion", "Tiger", "Cheetah"]
+
+for (let animal of animals) {
+
+    console.log(`"${animal} is a carnivorous animal"`)
+}
+
+//the common characterstic is:
+console.log(`"${animals} are carnivorous"`)
+
+
+console.log("Any of these animals  would make a great pet")
