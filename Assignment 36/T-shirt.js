1d18714af417d2321593b8d271db0ab01b7b1eaa
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-19 09:05:26 +0500

    added Assignment 36

---
+++ Assignment 36/T-shirt.js
@@ -0,0 +1,5 @@
+function make_shirt(size, text) {
+    console.log("The size of the T-shirt is: ".concat(size));
+    console.log("The text that has to be printed is: \"".concat(text, "\""));
+}
+make_shirt(18, "I LOVE PAKISTAN");
---
+++ Assignment 36/T-shirt.ts
@@ -0,0 +1,7 @@
+function make_shirt (size: number, text: string) {
+
+    console.log(`The size of the T-shirt is: ${size}`)
+    console.log(`The text that has to be printed is: "${text}"`)
+}
+
+make_shirt(18,"I LOVE PAKISTAN")
