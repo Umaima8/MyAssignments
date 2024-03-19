fbcfd2cfad847cc84ff38e74b2fdeb84babf005f
UmaimaFareed <umaimafaird88@gmail.com>
2024-03-19 09:01:51 +0500

    added Assignment 34

---
+++ Assignment 34/pizza.js
@@ -0,0 +1,9 @@
+var pizzas = ["cheese", "pepperoni", "BBQ chicken"];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("\"i love ".concat(pizza, " pizza\""));
+}
+console.log("\"I am very foodie and ".concat(pizzas[0], " pizza is my favourite\""));
+console.log("'".concat(pizzas[1], " pizza  has vey rich flavours'"));
+console.log("'".concat(pizzas[2], " pizza is very good in taste'"));
+console.log("i really like pizza");
---
+++ Assignment 34/pizza.ts
@@ -0,0 +1,12 @@
+let pizzas: string[] = ["cheese" , "pepperoni", "BBQ chicken" ]
+
+for (let pizza of pizzas) {
+    console.log(`"i love ${pizza} pizza"`)
+}
+
+console.log(`"I am very foodie and ${pizzas[0]} pizza is my favourite"`)
+console.log(`'${pizzas[1]} pizza  has vey rich flavours'`)
+console.log(`'${pizzas[2]} pizza is very good in taste'`)
+console.log("i really like pizza")
+
+
